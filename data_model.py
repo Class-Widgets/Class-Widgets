7ac0e18f1d7f9bea6ca2df819cf6c09bff17b599
Pizero <zhaory200707@outlook.com>
2025-07-01 19:25:16 +0800

    feat: new error style & more validator

--- data_model.py
+++ data_model.py
@@ -36,7 +36,7 @@ def validate_cses_time(time: str) -> str:
     regex = r"([01]\d|2[0-3]):([0-5]\d):([0-5]\d)"
     if match(regex, time):
         return time
-    raise ValueError(f"Invalid time format: {repr(time)}, need {repr(regex)}")
+    raise ValueError({"need": repr(regex), "got": time})
 
 
 class CsesClass(BaseModel):
@@ -51,6 +51,20 @@ class CsesSchedule(BaseModel):
     weeks: Literal["all", "odd", "even"]
     classes: List[CsesClass]
 
+    @model_validator(mode="after")
+    def validate_time(self) -> Self:
+        def to_offset(it: str) -> int:
+            tmp = it.split(":")
+            return int(tmp[0]) * 60 * 60 + int(tmp[1]) * 60 + int(tmp[2])
+
+        offsets = list(
+            (to_offset(class_.start_time), to_offset(class_.end_time))
+            for class_ in self.classes
+        )
+        if any(x[0] <= y[0] < x[1] for x in offsets for y in offsets):
+            raise ValueError({"conflict": "a class start time in another class"})
+        return self
+
 
 class Cses(BaseModel):
     version: Literal[1]
@@ -64,8 +78,34 @@ class Cses(BaseModel):
             class_.subject for schedule in self.schedules for class_ in schedule.classes
         }
         if forget_subject := (classes_name_set - sujects_name_set):
-            err_msg = "、".join(repr(name) for name in forget_subject)
-            raise ValueError(f"缺少 {err_msg} 课")
+            raise ValueError({"forget": {"subjects name": forget_subject}})
+        return self
+
+    @model_validator(mode="after")
+    def validate_schedule_weeks_enable_day(self) -> Self:
+        count_map: Dict[Tuple[str, int], List[str]] = dict()
+        for schedule in self.schedules:
+            current_id = (schedule.weeks, schedule.enable_day)
+            current_status = count_map.get(current_id, [])
+            count_map[current_id] = current_status + [schedule.name]
+        conflict_map = dict(
+            (id, names) for id, names in count_map.items() if len(names) > 1
+        )
+        if len(conflict_map) != 0:
+            raise ValueError({"conflict": {"weeks & enable_day": conflict_map}})
+        return self
+
+    @model_validator(mode="after")
+    def validate_subject_name(self) -> Self:
+        count_map: Dict[str, int] = dict()
+        for subject in self.subjects:
+            current_status = count_map.get(subject.name, 0)
+            count_map[subject.name] = current_status + 1
+        conflicts = list(
+            id for id, count in count_map.items() if count > 1
+        )
+        if len(conflicts) != 0:
+            raise ValueError({"conflict": {"subject name": conflicts}})
         return self
 
 
@@ -80,9 +120,19 @@ class Schedule(BaseModel):
     schedule_even: Dict[Literal["0", "1", "2", "3", "4", "5", "6"], List[str]]
 
     @model_validator(mode="after")
-    def validate_schedule(self) -> Self:
+    def validate_part_name(self) -> Self:
         if no_name_part := set(self.part.keys()) - set(self.part_name.keys()):
-            raise ValueError(
-                f"缺少 {'、'.join(repr(name) for name in no_name_part)} 的名称"
-            )
+            raise ValueError({"forget": {"part name": no_name_part}})
+        return self
+
+    @model_validator(mode="after")
+    def validate_dict_name(self) -> Self:
+        days = set(["0", "1", "2", "3", "4", "5", "6"])
+        if forget_day := days - self.schedule.keys():
+            raise ValueError({"forget": {"schedule key": forget_day}})
+        if forget_day := days - self.schedule_even.keys():
+            raise ValueError({"forget": {"schedule_even key": forget_day}})
+        timeline_day = set(["default", "0", "1", "2", "3", "4", "5", "6"])
+        if forget_day := timeline_day - self.timeline.keys():
+            raise ValueError({"forget": {"timeline key": forget_day}})
         return self
