37838a8a1d7e3c900be5a4a2e45fee68a5986683
HPD <120182813+IsHPDuwu@users.noreply.github.com>
2025-07-10 18:58:36 +0200

    Translated using Weblate (English (United States))
    Currently translated at 100.0% (703 of 703 strings)
    
    Translation: Class Widgets/cw1-view
    Translate-URL: https://hosted.weblate.org/projects/class-widgets-1/cw1/cw1-view/en_US/

--- i18n/en_US.ts
+++ i18n/en_US.ts
@@ -4260,7 +4260,7 @@ Do you want to hide the widget?</translation>
         <location filename="../main.py" line="2903"/>
         <source>当前缩放系数为 {scale_factor}%，可能导致显示异常。
 建议将缩放系数设置为 100% 到 180% 之间。</source>
-        <translation>Current sacaling factor is {scale_factor}% now, and it may cause display problems.
+        <translation>Current scaling factor is {scale_factor}% now, and it may cause display problems.
 It is recommended to set the scaling factor to a value between 100% and 180%.</translation>
     </message>
     <message>
