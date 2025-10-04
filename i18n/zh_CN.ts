cc7a78560762c13e57bf54c3676216fa516aaba5
Hosted Weblate <hosted@weblate.org>
2025-10-04 08:02:13 +0200

    Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 100.0% (828 of 828 strings)
    
    Co-authored-by: HPD <120182813+IsHPDuwu@users.noreply.github.com>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/class-widgets-1/cw1/cw1-view/zh_Hans/
    Translation: Class Widgets/cw1-view

--- i18n/zh_CN.ts
+++ i18n/zh_CN.ts
@@ -4312,7 +4312,7 @@
         <location filename="../main.py" line="3816"/>
         <location filename="../main.py" line="3822"/>
         <source>初始化窗口焦点监视器...</source>
-        <translation type="unfinished"></translation>
+        <translation>初始化窗口焦点监视器...</translation>
     </message>
     <message>
         <location filename="../main.py" line="3828"/>
@@ -4332,17 +4332,17 @@
     <message>
         <location filename="../main.py" line="3853"/>
         <source>检查临时课表...</source>
-        <translation type="unfinished"></translation>
+        <translation>检查临时课表...</translation>
     </message>
     <message>
         <location filename="../main.py" line="3858"/>
         <source>存在临时课表</source>
-        <translation type="unfinished"></translation>
+        <translation>存在临时课表</translation>
     </message>
     <message>
         <location filename="../main.py" line="3859"/>
         <source>当前存在临时课表，是否沿用</source>
-        <translation type="unfinished"></translation>
+        <translation>当前存在临时课表，是否沿用</translation>
     </message>
     <message>
         <location filename="../main.py" line="3869"/>
